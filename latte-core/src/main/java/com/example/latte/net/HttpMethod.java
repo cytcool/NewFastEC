bb1a5fae614c4fca7336bc2bab744e08108495d2
cytcool <675149948@qq.com>
2018-04-20 20:49:46 +0800

    网络框架接口创建

---
+++ latte-core/src/main/java/com/example/latte/net/HttpMethod.java
@@ -0,0 +1,15 @@
+package com.example.latte.net;
+
+/**
+ * Created by CYT on 2018/4/20.
+ */
+
+public enum HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UPLOAD
+}
---
+++ latte-core/src/main/java/com/example/latte/net/RestClient.java
@@ -0,0 +1,9 @@
+package com.example.latte.net;
+
+/**
+ * Created by CYT on 2018/4/20.
+ */
+
+public class RestClient {
+
+}
