86c4e2607f089b5b1d1d0276b8326711565b7acc
cytcool <675149948@qq.com>
2018-05-28 21:47:34 +0800

    初步实现底部导航BottomBar

---
+++ latte-core/src/main/java/com/example/latte/delegates/bottom/BottomTabBean.java
@@ -0,0 +1,20 @@
+package com.example.latte.delegates.bottom;
+
+public final class BottomTabBean {
+
+    private final CharSequence ICON;
+    private final CharSequence TITLE;
+
+    public BottomTabBean(CharSequence icon, CharSequence title) {
+        this.ICON = icon;
+        this.TITLE = title;
+    }
+
+    public CharSequence getICON() {
+        return ICON;
+    }
+
+    public CharSequence getTITLE() {
+        return TITLE;
+    }
+}
