6cc53338aebfd0aaa129c4a43db5eea4da5512f9
cytcool <675149948@qq.com>
2018-05-30 19:21:38 +0800

    RecyclerView数据解析

---
+++ latte-ec/src/main/java/com/example/latte/ec/main/index/IndexDataConverter.java
@@ -0,0 +1,14 @@
+package com.example.latte.ec.main.index;
+
+import com.example.latte.ui.recycler.DataConverter;
+import com.example.latte.ui.recycler.MultipleItemEntity;
+
+import java.util.ArrayList;
+
+public class IndexDataConverter extends DataConverter {
+
+    @Override
+    public ArrayList<MultipleItemEntity> conver() {
+        return null;
+    }
+}
